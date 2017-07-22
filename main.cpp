3d506fc0c2ec7041cd114c88510705e027176fd5
haodizc <haodizc@umich.edu>
2017-07-22 04:59:57 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -8,26 +8,30 @@ using namespace std;
 
 void print_map(vector<vector<int>> &map, int w, int h);
 void print_bool_map(vector<vector<bool>> &bool_map, int row, int col);
-int matrix_min_element(vector<vector<int>> &map);
-int matrix_max_element(vector<vector<int>> &map);
+Coordinate convert_coordinate(int x, int y, int h);
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"      /* Red */
 #define BLUE    "\033[34m"      /* Blue */
 
-int main() {
+struct Coordinate {
+    int x;
+    int y;
+};
 
+int main() {
     // https://nuro.ai/techfair
 
-    // (1, 4), (2, 1), (4, 5), and (5, 0)
+    // (1, 2), (2, 1), (4, 5), and (5, 0)
+    // (1, 4), (2, 5), (4, 1), and (5, 6)
 
     ifstream ifs("inputs_sample.txt");
     unsigned c, col, row, g;
     ifs >> c;
-    cout << c << endl;
+    cout << c << endl << endl;
     while (c--) {
         ifs >> col >> row >> g;
-        cout << col << " " << row << " " << g << endl;
+        cout << "W: " << col << "\t" << "H: " << row << "\t" << "G: " << g << endl;
         int num;
         vector<vector<int>> map(row, vector<int>(col, 0));
         for (int i = 0; i < row; i++) {
@@ -39,54 +43,9 @@ int main() {
 
         print_map(map, row, col);
 
-        auto min_element = matrix_min_element(map);
-        auto max_element = matrix_max_element(map);
-        cout << "Min element: " << min_element << endl;
-        cout << "Max element: " << max_element << endl << endl;
 
         vector<vector<bool>> bool_map(row, vector<bool>(col, true));
 
-        std::map<int,int> row_to_min_count;
-        std::map<int,int> row_to_max_count;
-        for (int i = 0; i < row; i++) {
-            row_to_min_count[i] = 0;
-            row_to_max_count[i] = 0;
-            for (int j = 0; j < col; j++) {
-                if (map[i][j] == min_element) {
-                    row_to_min_count[i]++;
-                }
-                if (map[i][j] == max_element) {
-                    row_to_max_count[i]++;
-                }
-            }
-//            cout << "Row " << i << " min count: " << row_to_min_count[i] << endl;
-        }
-//        cout << endl;
-
-        std::map<int,int> col_to_min_count;
-        std::map<int,int> col_to_max_count;
-        for (int j = 0; j < col; j++) {
-            col_to_min_count[j] = 0;
-            col_to_max_count[j] = 0;
-            for (int i = 0; i < row; i++) {
-                if (map[i][j] == min_element) {
-                    col_to_min_count[j]++;
-                }
-                if (map[i][j] == max_element) {
-                    col_to_max_count[j]++;
-                }
-            }
-//            cout << "Col " << j << " min count: " << col_to_min_count[j] << endl;
-        }
-//        cout << endl;
-
-//        for (int i = 0; i < row; i++) {
-//            if (row_to_max_count[i] == 0) {
-//                for (int j = 0; j < col; j++) {
-//                    bool_map[i][j] = false;
-//                }
-//            }
-//        }
 
         print_bool_map(bool_map, row, col);
     }
@@ -118,22 +77,11 @@ void print_bool_map(vector<vector<bool>> &bool_map, int row, int col) {
     cout << endl;
 }
 
-int matrix_min_element(vector<vector<int>> &map) {
-    std::vector<int>::iterator min_it;
-    int min = std::numeric_limits<int>::max();
-    for (size_t i = 0; i < map.size(); i++) {
-        min_it = std::min_element(map[i].begin(), map[i].end());
-        min = *min_it < min ? *min_it : min;
-    }
-    return min;
-}
+Coordinate convert_coordinate(int x, int y, int h) {
+    // 7x7
+    // (1, 2), (2, 1), (4, 5), and (5, 0)
+    // (1, 4), (2, 5), (4, 1), and (5, 6)
 
-int matrix_max_element(vector<vector<int>> &map) {
-    std::vector<int>::iterator max_it;
-    int max = 0;
-    for (size_t i = 0; i < map.size(); i++) {
-        max_it = std::max_element(map[i].begin(), map[i].end());
-        max = *max_it > max ? *max_it : max;
-    }
-    return max;
+    Coordinate c = {x, h - 1 - y};
+    return c;
 }
